dae87a701392f795af4c08b515127d5aed1b6029
eric <eric.thomas@formation-industries-fc.fr>
2020-12-16 16:23:40 +0100

    final V2 with comment

--- untitled1.ino
+++ untitled1.ino
@@ -1,33 +1,33 @@
 #include <Arduino.h>
-
-int pinLeds[4]={13,12,11,10};
-int pinButtons[2]={7,6};
+/******************************ensemble des pins sur l'arduino********************/
+int pinLeds[4]={13,12,11,10}; //tableau de 4 cases entier rempli avec 13,12,11,10
+int pinButtons[2]={7,6};  //tableau de 2 cases rempli avec 7 et 6
 
 void setup()
 {
     /***************initialisation pin leds*************/
     for(int i=0;i<4;i++)
     {
-        pinMode(pinLeds[i],OUTPUT);
+        pinMode(pinLeds[i],OUTPUT); //met les pins de pinleds en mode sortie
 
     }
     /************initialisation pin buttons**************/
     for(int i=0;i<2;i++)
     {
-        pinMode(pinButtons[i],INPUT);
+        pinMode(pinButtons[i],INPUT); //met les pins de pinbuttons en mode entrée
     }
 
 }
-
+/**************traitement du bouton1 à ON************************/
 void button1On()
 {
     for(int i=0;i<4;i++)
     {
-        if(i==1)
+        if(i==1) //sauf la led de la case1
         {
             continue;
         }
-        digitalWrite(pinLeds[i],1);
+        digitalWrite(pinLeds[i],1); //on allume toutes les leds
     }
 }
 
@@ -35,7 +35,7 @@ void buttonOff()
 {
     for(int i=0;i<4;i++)
     {
-        digitalWrite(pinLeds[i],0);
+        digitalWrite(pinLeds[i],0); //on éteint toutes les leds
     }
 }
 
@@ -43,33 +43,33 @@ void button2On()
 {
     for(int i=0;i<4;i++)
     {
-        if(i==2)
+        if(i==2) //sauf la led de la case 2
         {
             continue;
         }
-        digitalWrite(pinLeds[i],1);
+        digitalWrite(pinLeds[i],1); //on allume toutes les leds
     }
 }
 void loop()
 {
-
-    if(digitalRead(pinButtons[0])==0)
+/*******************code avec montage sur le + ************************/
+    if(digitalRead(pinButtons[0])==0) //si le bouton 1 est appuyé
     {
-        button1On();
+        button1On(); //appel la fonction buttonON
     }
     else
-    if(digitalRead(pinButtons[0])==1)
+    if(digitalRead(pinButtons[0])==1) //si le bouton est relaché
     {
-        buttonOff();
+        buttonOff(); //appel de la fonction buttonOFF
     }
-    if(digitalRead(pinButtons[1])==0)
+    if(digitalRead(pinButtons[1])==0) //si le bouton 2 est appuyé
     {
-        button2On();
+        button2On(); //appel de la fonction buttonON
     }
     else
-    if(digitalRead(pinButtons[1])==1)
+    if(digitalRead(pinButtons[1])==1) //si le bouton est relaché
     {
-        buttonOff();
+        buttonOff(); //appel de la fonction buttonOFF
     }
 
 
